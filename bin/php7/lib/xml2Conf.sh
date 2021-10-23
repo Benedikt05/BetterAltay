5926f1871667a527922f3751059ba01961d62818
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2021-10-23 21:20:05 +0200

    Delete useless file

--- bin/php7/lib/xml2Conf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the XML library in GNOME applications
-#
-XML2_LIBDIR="-L/home/vsts/work/1/s/bin/php7/lib"
-XML2_LIBS="-lxml2 -L/home/vsts/work/1/s/bin/php7/lib -lz     -lm "
-XML2_INCLUDEDIR="-I/home/vsts/work/1/s/bin/php7/include/libxml2"
-MODULE_VERSION="xml2-2.9.12"
-
