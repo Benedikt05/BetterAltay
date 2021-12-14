00a611c02df1a8ba6127e708d6ea2ddd4e4aa4a9
DRAGKILLS <skywarsmcpe2@gmail.com>
2021-12-14 08:29:39 +0300

    esko release

--- build/server-phar.php
+++ build/server-phar.php
@@ -147,7 +147,7 @@ function main() : void{
 		$build = 0;
 	}
 	foreach(buildPhar(
-		$opts["out"] ?? getcwd() . DIRECTORY_SEPARATOR . "BetterAltay.phar",
+		$opts["out"] ?? getcwd() . DIRECTORY_SEPARATOR . "EskoBE.phar",
 		dirname(__DIR__) . DIRECTORY_SEPARATOR,
 		[
 			'src',
