1ecd33e94eec15cd3fc5878fc692e87d12b65815
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2021-12-17 19:52:05 +0100

    3.26.3

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -34,6 +34,6 @@ const _VERSION_INFO_INCLUDED = true;
 
 const NAME = "BetterAltay";
 const BASE_VERSION = "3.26.3";
-const FORK_VERSION = "1.0.0";
-const IS_DEVELOPMENT_BUILD = true;
+const FORK_VERSION = "1.0.1";
+const IS_DEVELOPMENT_BUILD = false;
 const BUILD_CHANNEL = "master";
