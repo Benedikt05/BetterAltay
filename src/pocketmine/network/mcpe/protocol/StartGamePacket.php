836787b669b2776bf6a9acbb80d87edbfd63d980
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2021-12-19 21:22:39 +0100

    maybe this will fix end/nether

--- src/pocketmine/network/mcpe/protocol/StartGamePacket.php
+++ src/pocketmine/network/mcpe/protocol/StartGamePacket.php
@@ -137,7 +137,7 @@ class StartGamePacket extends DataPacket{
 	/** @var bool */
 	public $onlySpawnV1Villagers = false;
 	/** @var string */
-	public $vanillaVersion = ProtocolInfo::MINECRAFT_VERSION_NETWORK;
+	public $vanillaVersion = 471;
 	/** @var int */
 	public $limitedWorldWidth = 0;
 	/** @var int */
