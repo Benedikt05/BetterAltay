5b9d00e216e23381499edef2304dff5b2157750f
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2022-01-05 00:28:10 +0100

    Update VersionInfo.php

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -34,6 +34,6 @@ const _VERSION_INFO_INCLUDED = true;
 
 const NAME = "BetterAltay";
 const BASE_VERSION = "3.26.4";
-const FORK_VERSION = "1.0.1";
-const IS_DEVELOPMENT_BUILD = true;
-const BUILD_CHANNEL = "Feature";
+const FORK_VERSION = "1.0.2";
+const IS_DEVELOPMENT_BUILD = false;
+const BUILD_CHANNEL = "master";
