d65cee979dde1fb40068e47ffd81382316559a98
VaxPex <vaxpex11@gmail.com>
2022-02-09 12:23:17 +0300

    some protocol changes

--- src/pocketmine/inventory/ShapedRecipe.php
+++ src/pocketmine/inventory/ShapedRecipe.php
@@ -176,9 +176,6 @@ class ShapedRecipe implements CraftingRecipe{
 		return $this->shape;
 	}
 
-	/**
-	 * @deprecated
-	 */
 	public function registerToCraftingManager(CraftingManager $manager) : void{
 		$manager->registerShapedRecipe($this);
 	}
