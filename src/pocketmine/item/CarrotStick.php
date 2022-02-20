585037ef2a3373069b2c1d308c15d598f231979a
MCPE357 <89349227+MCPE357@users.noreply.github.com>
2022-02-20 14:52:39 +0200

    Add files via upload

---
+++ src/pocketmine/item/CarrotStick.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace pocketmine\item;
+
+class CarrotStick extends Item
+{
+
+	public function getMaxStackSize(): int
+	{
+		return 1;
+	}
+}
---
+++ src/pocketmine/item/FungusStick.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace pocketmine\item;
+
+class FungusStick extends Item
+{
+
+	public function getMaxStackSize(): int
+	{
+		return 1;
+	}
+}
