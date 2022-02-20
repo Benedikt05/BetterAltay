5e7782dfa2977cd35b9cc9708057b5262d768742
MCPE357 <89349227+MCPE357@users.noreply.github.com>
2022-02-20 14:07:11 +0200

    Add end portal

--- src/pocketmine/block/EndPortalFrame.php
+++ src/pocketmine/block/EndPortalFrame.php
@@ -24,6 +24,8 @@ declare(strict_types=1);
 namespace pocketmine\block;
 
 use pocketmine\item\Item;
+use pocketmine\math\Vector3;
+use pocketmine\Player;
 use pocketmine\math\AxisAlignedBB;
 
 class EndPortalFrame extends Solid{
@@ -64,5 +66,53 @@ class EndPortalFrame extends Solid{
 			$this->y + (($this->getDamage() & 0x04) > 0 ? 1 : 0.8125),
 			$this->z + 1
 		);
+	}
+	
+	public function place(Item $item, Block $block, Block $target, int $face, Vector3 $facePos, Player $player = null): bool{
+		$faces = [
+			0 => 3,
+			1 => 0,
+			2 => 1,
+			3 => 2,
+		];
+		$this->meta = $faces[$player instanceof Player ? $player->getDirection() : 0];
+		$this->getLevel()->setBlock($block, $this, true, true);
+
+		return true;
+	}
+
+	public function onActivate(Item $item, Player $player = null): bool{
+		if(($this->getDamage() & 0x04) === 0 && $player instanceof Player && $item->getId() === Item::ENDER_EYE){
+			$this->setDamage($this->getDamage() + 4);
+			$this->getLevel()->setBlock($this, $this, true, true);
+			return true;
+		}
+		return false;
+	}
+
+	public function isValidPortal(): array{
+		return [
+			new Vector3(0, 0, 0),
+			new Vector3(0, 0, 0),
+			new Vector3(0, 0, 0),
+			new Vector3(0, 0, 0),
+		];
+	}
+	
+	private function createPortal(array $corners = null): bool{
+		if($corners === null){
+			return false;
+		}
+		$x1 = min($corners[0][0], $corners[1][0]);
+		$x2 = max($corners[0][0], $corners[1][0]);
+		$z1 = min($corners[0][1], $corners[1][1]);
+		$z2 = max($corners[0][1], $corners[1][1]);
+		$y = $corners[2];
+		for($curX = $x1; $curX <= $x2; $curX++){
+			for($curZ = $z1; $curZ <= $z2; $curZ++){
+				$this->getLevel()->setBlock($player->asVector3()->add(curX, $y, $curZ), Block::get(119));
+			}
+		}
+		return true;
 	}
 }
