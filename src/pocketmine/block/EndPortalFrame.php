2e6450bfc53ffd2f5f03b3e0948ad085951bd289
VaxPex <vaxpex11@gmail.com>
2022-03-07 15:20:33 +0300

    a

--- src/pocketmine/block/EndPortalFrame.php
+++ src/pocketmine/block/EndPortalFrame.php
@@ -71,8 +71,8 @@ class EndPortalFrame extends Solid{
 			$this->z + 1
 		);
 	}
-	
-	public function place(Item $item, Block $block, Block $target, int $face, Vector3 $facePos, Player $player = null): bool{
+
+	public function place(Item $item, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector, Player $player = null): bool{
 		$faces = [
 			0 => 3,
 			1 => 0,
@@ -80,7 +80,7 @@ class EndPortalFrame extends Solid{
 			3 => 2,
 		];
 		$this->meta = $faces[$player instanceof Player ? $player->getDirection() : 0];
-		$this->getLevel()->setBlock($block, $this, true, true);
+		$this->getLevel()->setBlock($blockReplace, $this, true, true);
 		return true;
 	}
 
