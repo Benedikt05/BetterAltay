265f9f5beb4d983bd498dba6a8726469a5191cd5
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2022-12-12 17:14:31 +0100

    fixes

--- src/pocketmine/block/SnowLayer.php
+++ src/pocketmine/block/SnowLayer.php
@@ -23,6 +23,7 @@ declare(strict_types=1);
 
 namespace pocketmine\block;
 
+use pocketmine\entity\Entity;
 use pocketmine\item\Item;
 use pocketmine\item\ItemFactory;
 use pocketmine\item\TieredTool;
