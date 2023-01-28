59a5018fc5b4cc20c7146276fe179514afe6de78
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2023-01-28 02:29:20 +0100

    1.19.60: joining works!

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -37,11 +37,11 @@ interface ProtocolInfo{
 	 */
 
 	/** Actual Minecraft: PE protocol version */
-	public const CURRENT_PROTOCOL = 560;
+	public const CURRENT_PROTOCOL = 567;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
-	public const MINECRAFT_VERSION = 'v1.19.50';
+	public const MINECRAFT_VERSION = 'v1.19.60';
 	/** Version number sent to clients in ping responses. */
-	public const MINECRAFT_VERSION_NETWORK = '1.19.50';
+	public const MINECRAFT_VERSION_NETWORK = '1.19.60';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;
