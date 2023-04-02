dbdc78a6daf41066303f046b7628422e626a076f
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2023-04-02 21:33:41 +0200

    Update ProtocolInfo.php

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -37,7 +37,7 @@ interface ProtocolInfo{
 	 */
 
 	/** Actual Minecraft: PE protocol version */
-	public const CURRENT_PROTOCOL = 575;
+	public const CURRENT_PROTOCOL = self::PROTOCOL_1_19_70;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
 	public const MINECRAFT_VERSION = 'v1.19.30 - 1.19.70';
 	/** Version number sent to clients in ping responses. */
