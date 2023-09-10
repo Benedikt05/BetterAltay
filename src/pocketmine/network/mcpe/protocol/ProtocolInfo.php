e67039fef1a94a2986381decde0cf3047317b6b8
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2023-09-10 03:41:22 +0200

    start working on 1.20.30 support

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -37,11 +37,11 @@ interface ProtocolInfo{
 	 */
 
 	/** Actual Minecraft: PE protocol version */
-	public const CURRENT_PROTOCOL = 594;
+	public const CURRENT_PROTOCOL = 617;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
 	public const MINECRAFT_VERSION = 'v' . self::MINECRAFT_VERSION_NETWORK;
 	/** Version number sent to clients in ping responses. */
-	public const MINECRAFT_VERSION_NETWORK = '1.20.10';
+	public const MINECRAFT_VERSION_NETWORK = '1.20.30';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;
