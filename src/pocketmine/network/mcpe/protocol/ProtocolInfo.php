27fbc46f0f1e5b58cd5c2eb3ae9ed14fa986f184
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2023-09-19 19:04:07 +0200

    bump protocol version to 618

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -37,7 +37,7 @@ interface ProtocolInfo{
 	 */
 
 	/** Actual Minecraft: PE protocol version */
-	public const CURRENT_PROTOCOL = 617;
+	public const CURRENT_PROTOCOL = 618;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
 	public const MINECRAFT_VERSION = 'v' . self::MINECRAFT_VERSION_NETWORK;
 	/** Version number sent to clients in ping responses. */
