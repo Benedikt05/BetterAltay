9a07095a7204427394d720c8f2c5d20f2ba7991f
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2024-07-09 19:15:41 +0200

    bump protocol to 686

--- src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
+++ src/pocketmine/network/mcpe/protocol/ProtocolInfo.php
@@ -37,11 +37,11 @@ interface ProtocolInfo{
 	 */
 
 	/** Actual Minecraft: PE protocol version */
-	public const CURRENT_PROTOCOL = 685;
+	public const CURRENT_PROTOCOL = 686;
 	/** Current Minecraft PE version reported by the server. This is usually the earliest currently supported version. */
 	public const MINECRAFT_VERSION = 'v' . self::MINECRAFT_VERSION_NETWORK;
 	/** Version number sent to clients in ping responses. */
-	public const MINECRAFT_VERSION_NETWORK = '1.21.0';
+	public const MINECRAFT_VERSION_NETWORK = '1.21.2';
 
 	public const LOGIN_PACKET = 0x01;
 	public const PLAY_STATUS_PACKET = 0x02;
