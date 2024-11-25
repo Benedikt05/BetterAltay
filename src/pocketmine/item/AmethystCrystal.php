0a7a013e231a57f3356a4c4630af63a7fcd07890
DANRULEZ123 <40849132+DANRULEZ123@users.noreply.github.com>
2024-11-25 15:05:48 -0500

    Create AmethystCrystal.php

---
+++ src/pocketmine/item/AmethystCrystal.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace pocketmine\item;
+
+use pocketmine\block\Block;
+use pocketmine\Player;
+
+class AmethystCrystal extends Item {
+
+    public function __construct(int $meta = 0){
+        parent::__construct(self::AMETHYST_CRYSTAL, $meta, "Amethyst Crystal");
+    }
+
+    public function getMaxStackSize(): int {
+        return 64;
+    }
+    public function getName(): string{
+        return "Amethyst Crystal";
+}
+}
