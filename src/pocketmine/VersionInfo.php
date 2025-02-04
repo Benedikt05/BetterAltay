6f6f98d84e7d9ea0cb035f8d8b27d08e0ef41a49
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2025-02-04 16:39:31 +0100

    Update VersionInfo.php

--- src/pocketmine/VersionInfo.php
+++ src/pocketmine/VersionInfo.php
@@ -33,7 +33,7 @@ if(defined('pocketmine\_VERSION_INFO_INCLUDED')){
 const _VERSION_INFO_INCLUDED = true;
 
 const NAME = "BetterAltay";
-const BASE_VERSION = "3.28.0"; //Don't change this anymore. Change the FORK_VERSION instead.
-const FORK_VERSION = "1.27.0";
-const IS_DEVELOPMENT_BUILD = false;
+const BASE_VERSION = "3.28.0"; //Do not modify this. Change FORK_VERSION instead.
+const FORK_VERSION = "1.28.0";
+const IS_DEVELOPMENT_BUILD = true;
 const BUILD_CHANNEL = "master";
