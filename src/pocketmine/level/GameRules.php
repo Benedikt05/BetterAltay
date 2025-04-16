4aa53153a361a081731378abeca9e507dc1611a9
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2025-04-16 20:52:27 +0200

    fix locator bar constant name

--- src/pocketmine/level/GameRules.php
+++ src/pocketmine/level/GameRules.php
@@ -54,7 +54,7 @@ class GameRules{
 	public const RULE_NATURAL_REGENERATION = "naturalRegeneration";
 	public const RULE_RANDOM_TICK_SPEED = "randomtickspeed";
 	public const RULE_RECIPES_UNLOCK = "recipesunlock";
-	public const LOCATOR_BAR = "locatorBar";
+	public const RULE_LOCATOR_BAR = "locatorBar";
 
 	public const RULE_TYPE_BOOL = 1;
 	public const RULE_TYPE_INT = 2;
@@ -90,7 +90,7 @@ class GameRules{
 		$this->setBool(self::RULE_TNT_EXPLODES, true);
 		$this->setInt(self::RULE_RANDOM_TICK_SPEED, 3);
 		$this->setBool(self::RULE_RECIPES_UNLOCK, false);
-		$this->setBool(self::LOCATOR_BAR, false);
+		$this->setBool(self::RULE_LOCATOR_BAR, false);
 	}
 
 	/**
