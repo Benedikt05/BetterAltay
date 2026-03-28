8abddca74e65abc2890239ddf06e97c791b0f9ad
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2026-03-28 15:35:31 +0100

    feat: fully implement VoxelShapesPacket

--- src/pocketmine/network/mcpe/protocol/VoxelShapesPacket.php
+++ src/pocketmine/network/mcpe/protocol/VoxelShapesPacket.php
@@ -7,23 +7,48 @@ namespace pocketmine\network\mcpe\protocol;
 #include <rules/DataPacket.h>
 
 use pocketmine\network\mcpe\NetworkSession;
+use pocketmine\network\mcpe\protocol\types\SerializableVoxelShape;
 
 class VoxelShapesPacket extends DataPacket{
 	public const NETWORK_ID = ProtocolInfo::VOXEL_SHAPES_PACKET;
-
-	/*
-	 * public array $shapes = [];
-	 * public array $names = [];
-	*/
-	public int $customShapeCount = 0;
+	/**
+	 * @var SerializableVoxelShape[]
+	 * @phpstan-var list<SerializableVoxelShape>
+	 */
+	 public array $shapes = [];
+	/**
+	 * @var int[]
+	 * @phpstan-var array<string, int>
+	 */
+	 public array $names = [];
+	 public int $customShapeCount = 0;
 
 	protected function decodePayload() : void{
-		//TODO
+		$this->shapes = [];
+		for($i = 0; $i < $this->getUnsignedVarInt(); ++$i){
+			$this->shapes[] = SerializableVoxelShape::read($this);
+		}
+
+		$this->names = [];
+		for($i = 0; $i < $this->getUnsignedVarInt(); ++$i){
+			$name = $this->getString();
+			$id = $this->getLShort();
+			$this->names[$name] = $id;
+		}
+
+		$this->customShapeCount = $this->getLShort();
 	}
 
 	protected function encodePayload() : void{
-		$this->putUnsignedVarInt(0); //shapes
-		$this->putUnsignedVarInt(0); //names
+		$this->putUnsignedVarInt(count($this->shapes));
+		foreach($this->shapes as $shape){
+			$shape->write($this);
+		}
+		$this->putUnsignedVarInt(count($this->names));
+		foreach($this->names as $name => $id){
+			$this->putString($name);
+			$this->putLShort($id);
+		}
 		$this->putLShort($this->customShapeCount);
 	}
 
