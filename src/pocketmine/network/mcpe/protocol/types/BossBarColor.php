823c6c1ddb1cea8fe75d150b1ec14977e464ae8f
Alex <250800467+ByteForge88@users.noreply.github.com>
2026-04-17 17:06:37 -0700

    smh

--- src/pocketmine/network/mcpe/protocol/types/BossBarColor.php
+++ src/pocketmine/network/mcpe/protocol/types/BossBarColor.php
@@ -17,12 +17,12 @@ namespace pocketmine\network\mcpe\protocol\types;
 class BossBarColor {
 
     public const PINK = 0;
-	  public const BLUE = 1;
-	  public const RED = 2;
-	  public const GREEN = 3;
-	  public const YELLOW = 4;
-	  public const PURPLE = 5;
-	  public const REBECCA_PURPLE = 6;
-	  public const WHITE = 7;
+	public const BLUE = 1;
+	public const RED = 2;
+	public const GREEN = 3;
+	public const YELLOW = 4;
+	public const PURPLE = 5;
+	public const REBECCA_PURPLE = 6;
+	public const WHITE = 7;
 
 }
