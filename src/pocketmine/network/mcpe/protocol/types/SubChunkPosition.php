ab03e2e5a4bbdffac4b81e128aea336d7fdfd3f7
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2026-06-14 18:56:14 +0200

    Adapt SubChunkRequestPacket

--- src/pocketmine/network/mcpe/protocol/types/SubChunkPosition.php
+++ src/pocketmine/network/mcpe/protocol/types/SubChunkPosition.php
@@ -53,4 +53,18 @@ final class SubChunkPosition{
 		$out->putVarInt($this->y);
 		$out->putVarInt($this->z);
 	}
+
+	public static function readv1001(NetworkBinaryStream $in) : self{
+		$x = $in->getLInt();
+		$y = $in->getLInt();
+		$z = $in->getLInt();
+
+		return new self($x, $y, $z);
+	}
+
+	public function writev1001(NetworkBinaryStream $out) : void{
+		$out->putLInt($this->x);
+		$out->putLInt($this->y);
+		$out->putLInt($this->z);
+	}
 }
