a323be0faa35c6460c277e6fa141ec425db510fd
Benedikt05 <60869945+Benedikt05@users.noreply.github.com>
2026-08-19 18:06:57 +0200

    updates

--- src/pocketmine/network/mcpe/protocol/types/GatheringJoinInfo.php
+++ src/pocketmine/network/mcpe/protocol/types/GatheringJoinInfo.php
@@ -10,32 +10,32 @@ use pocketmine\utils\UUID;
 final class GatheringJoinInfo{
 
 	public function __construct(
-		private UUID $experienceId,
-		private string $experienceName,
-		private UUID $experienceWorldId,
-		private string $experienceWorldName,
-		private string $creatorId,
-		private UUID $targetId,
-		private string $scenarioId,
-		private string $serverId,
+		private ?UUID $experienceId,
+		private ?string $experienceName,
+		private ?UUID $experienceWorldId,
+		private ?string $experienceWorldName,
+		private ?string $creatorId,
+		private ?UUID $targetId,
+		private ?string $scenarioId,
+		private ?string $serverId,
 	){
 	}
 
-	public function getExperienceId() : UUID{ return $this->experienceId; }
+	public function getExperienceId() : ?UUID{ return $this->experienceId; }
 
-	public function getExperienceName() : string{ return $this->experienceName; }
+	public function getExperienceName() : ?string{ return $this->experienceName; }
 
-	public function getExperienceWorldId() : UUID{ return $this->experienceWorldId; }
+	public function getExperienceWorldId() : ?UUID{ return $this->experienceWorldId; }
 
-	public function getExperienceWorldName() : string{ return $this->experienceWorldName; }
+	public function getExperienceWorldName() : ?string{ return $this->experienceWorldName; }
 
-	public function getCreatorId() : string{ return $this->creatorId; }
+	public function getCreatorId() : ?string{ return $this->creatorId; }
 
-	public function getTargetId() : UUID{ return $this->targetId; }
+	public function getTargetId() : ?UUID{ return $this->targetId; }
 
-	public function getScenarioId() : string{ return $this->scenarioId; }
+	public function getScenarioId() : ?string{ return $this->scenarioId; }
 
-	public function getServerId() : string{ return $this->serverId; }
+	public function getServerId() : ?string{ return $this->serverId; }
 
 	public static function read(NetworkBinaryStream $in) : self{
 		$experienceId = $in->getUUID();
@@ -62,11 +62,11 @@ final class GatheringJoinInfo{
 	public function write(NetworkBinaryStream $out) : void{
 		$out->writeOptional($this->experienceId, fn($experienceId) => $out->putUUID($experienceId));
 		$out->writeOptional($this->experienceName, fn($experienceName) => $out->putString($experienceName));
-		$out->putUUID($this->experienceWorldId);
-		$out->putString($this->experienceWorldName);
+		$out->writeOptional($this->experienceWorldId, fn($experienceWorldId) => $out->putUUID($experienceWorldId));
+		$out->writeOptional($this->experienceWorldName, fn($experienceWorldName) => $out->putString($experienceWorldName));
 		$out->writeOptional($this->creatorId, fn($creatorId) => $out->putString($creatorId));
-		$out->putUUID($this->targetId);
-		$out->putString($this->scenarioId);
-		$out->putString($this->serverId);
+		$out->writeOptional($this->targetId, fn($targetId) => $out->putUUID($targetId));
+		$out->writeOptional($this->scenarioId, fn($scenarioId) => $out->putString($scenarioId));
+		$out->writeOptional($this->serverId, fn($serverId) => $out->putString($serverId));
 	}
 }
